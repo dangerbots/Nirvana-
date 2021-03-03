77f556a5fee114cb56bfa6c5fbc6e85ccbde91a1
Adithyan-kollam <73607487+piku-adhi@users.noreply.github.com>
2021-03-03 23:26:27 +0530

    Update __init__.py

--- AlexaSongBot/__init__.py
+++ AlexaSongBot/__init__.py
@@ -1,7 +1,7 @@
 # © @Mr_Dark_Prince
 import logging
 from pyrogram import Client
-from config import API_HASH, API_ID, BOT_TOKEN
+from config import API_HASH, API_ID, BOT_TOKEN, START_IMG
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -9,4 +9,4 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-app = Client("AlexaSongBot", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
+app = Client("AlexaSongBot", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID, start_img=START_IMG)
