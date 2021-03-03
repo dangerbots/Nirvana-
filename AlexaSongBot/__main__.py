43a6fd26fea248c31e03dc9f6ad9042d173358ae
Adithyan-kollam <73607487+piku-adhi@users.noreply.github.com>
2021-03-03 22:28:32 +0530

    Update __main__.py

--- AlexaSongBot/__main__.py
+++ AlexaSongBot/__main__.py
@@ -13,7 +13,7 @@ start_text = """
 Hey [{}](tg://user?id={}),
 I'm Nirvana😌
 Just send me the song name you want to download.i will search on YouTube and \n i will find it to you
-example:- /music song name {valid YouTube name}
+example:- /music song name [valid YouTube name]
 """
 
 owner_help = """
