1089ff31c7096ec3892488293b26398702a64d03
Adithyan-kollam <73607487+piku-adhi@users.noreply.github.com>
2021-03-04 15:06:33 +0530

    Update __main__.py

--- AlexaSongBot/__main__.py
+++ AlexaSongBot/__main__.py
@@ -12,13 +12,10 @@ from AlexaSongBot.sql.chat_sql import add_chat_to_db
 start_text = """
 Hey [{}](tg://user?id={}),
 
-
 I'm Nirvana[😌](https://telegra.ph/file/d9779732beb31e470b763.jpg)
 
-
-
-
 Just send me the song name you want to download.i will search on YouTube and \n i will find it to you
+
 example:- /music song name [valid YouTube name]
 """
 
